99e16550befab1bd70fa90019cea8ee1f24292c9
Julio Gomez <julio.gomez@findero.mx>
2021-03-16 12:00:42 -0600

    Hipervinculos funcionando creo
    No lo pude probar aún porque se generan columnas de más

--- Hipervinculos.py
+++ Hipervinculos.py
@@ -28,22 +28,27 @@ def hipervinculos(Cliente):
     Res[['A', 'B']] = Res[['A', 'B']].fillna(method = 'pad')
     Des = pd.read_excel(archivo_resultados, sheet_name='Desciframiento')
     Des.columns = Dicc
-
-
     Des.drop([0,1,2,3,4],inplace=True)
     Des.dropna(subset=['C'],inplace=True)
     Res.drop(Res.index[:9], inplace=True)
     # Des = Des.replace('C', '')
-    circuitoD = Des['C'].str.split(' ', 1)
+    Res = Res[Res.A != 'Periodo:']
+    Res = Res[Res.B != 'Tablero']
+    #print(Res)
+    Des['C'] = Des['C'].str.capitalize()
+    circuitoD = Des['C'][Des.C != 'Ubicacion']
+    circuitoD = circuitoD.str.split(' ', 1)
     Res.dropna(subset=['D'], inplace=True)
     Res['D'] = Res['D'].astype(str)
     circuitoR = Res[['B', 'D']]
+    #print(circuitoR)
+    valorT = len(set(Res['B']))
+    print(valorT)
+    #print(len(valorT))
+    circuitoR['B'] = circuitoR['B'].str.lower()
     circuitoR['D'] = circuitoR['D'].str.split().str.get(0)
     workbook = xlwings.Book(archivo_resultados)
     Sheet1 = workbook.sheets['Desciframiento']
-    circuitoR.groupby(by="B")
-    print(circuitoD)
-    print(circuitoR)
     for i in circuitoD.index:
         try:
             circ=int(circuitoD[i][0][1:])
@@ -52,13 +57,23 @@ def hipervinculos(Cliente):
             
         except:
             circ = circuitoD[i][0][1:]
+            tab = circuitoD[i][1][0:]
             donde = circuitoR[circuitoR== circ].index
-
-        if not donde.empty:
-            Puerto =Res.loc[donde.values,'C'].values[0]
-            Findero=Res.loc[donde.values, 'A'].values[0]
-            Hvin= '../Graficas/Consumo/'+ str(Findero) +'/'  + str(Findero) +'Puerto ' + str(Puerto) +'.html'
-            Sheet1.range(i+2,3).add_hyperlink(Hvin,str(Des.loc[i,'C']))
-
+        if valorT > 3:
+            if not donde.empty:
+                for j in donde.values:
+                    if circuitoR['B'][j]==tab and circuitoR['D'][j]==circ:
+                        Puerto =Res['C'][j]
+                        Findero=Res['A'][j]
+                        Hvin= '../Graficas/Consumo/'+ str(Findero) +'/'  + str(Findero) +'Puerto ' + str(Puerto) +'.html'
+                        Sheet1.range(i+2,3).add_hyperlink(Hvin,str(Des.loc[i,'C']))
+        else:
+           if not donde.empty:
+               for j in donde.values:
+                   if circuitoR['D'][j]==circ:
+                       Puerto =Res['C'][j]
+                       Findero=Res['A'][j]
+                       Hvin= '../Graficas/Consumo/'+ str(Findero) +'/'  + str(Findero) +'Puerto ' + str(Puerto) +'.html'
+                       Sheet1.range(i+2,3).add_hyperlink(Hvin,str(Des.loc[i,'C']))
     workbook.save()
     workbook.close()
