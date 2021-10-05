8988f72cd6f8e9feb26fb77acceb0eff879a6333
emiliodiam <42275672+emiliodiam@users.noreply.github.com>
2021-10-05 11:40:41 -0500

    librerias dispensadores y maquinas de hielo

---
+++ libreriaHielo.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import funcionesComunes as fc
+
+def leerLibreria():
+    try:
+        lib = pd.read_excel(
+            f"../../../Recomendaciones de eficiencia energetica/Librerias/Maquinas de hielo/libreriaMaquinasHielo.xlsx",
+            sheet_name='libreriaHielo')
+        links = pd.read_excel(
+            f"../../../Recomendaciones de eficiencia energetica/Librerias/Maquinas de hielo/libreriaMaquinasHielo.xlsx",
+            sheet_name='links')
+    except:
+        lib = pd.read_excel(
+            f"D:/Findero Dropbox/Recomendaciones de eficiencia energetica/Librerias/Maquinas de hielo/libreriaMaquinasHielo.xlsx",
+            sheet_name='libreriaHielo')
+        links = pd.read_excel(
+            f"D:/Findero Dropbox/Recomendaciones de eficiencia energetica/Librerias/Maquinas de hielo/libreriaMaquinasHielo.xlsx",
+            sheet_name='links')
+    return lib, links
+def recoDispensadores(kwh):
+    lib, links = leerLibreria()
+
+    if kwh<=20:
+        txt = fc.selecTxt(lib, "HIEL01")
+    else:
+        txt = fc.selecTxt(lib, "HIEL02").replace("[TIMER INTELIGENTE]",fc.ligarTextolink("Timer inteligente",links.at[0,"Link"]))
+    return txt
