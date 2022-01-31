4c8187bd09892f766a1f831922ac7fa5ea8770b3
GerardoFindero <48810049+GerardoFindero@users.noreply.github.com>
2022-01-31 11:02:46 -0600

    Cambios310122
    CAmbios varios

--- LeerEncuesta.py
+++ LeerEncuesta.py
@@ -112,8 +112,14 @@ def Nombre_Cliente():
 
     #NCliente = Cliente_Prueba()
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     NCliente = "Marianela Hernandez"
     #NCliente = "Rodolfo Hill"
+=======
+    #NCliente = "Prueba Lista"
+    #NCliente = "Salvador Medina"
+    #NCliente = "Samuel N"
+>>>>>>> Stashed changes
 =======
     #NCliente = "Prueba Lista"
     #NCliente = "Salvador Medina"
@@ -143,7 +149,11 @@ if __name__ == '__main__':
 ## Se elige la opción del programa que se quiere correr
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     Opcion='4'
+=======
+    Opcion='3'
+>>>>>>> Stashed changes
 =======
     Opcion='3'
 >>>>>>> Stashed changes
